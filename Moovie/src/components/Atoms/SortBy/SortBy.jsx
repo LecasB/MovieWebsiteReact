d21258167ac9b4049c1f4d204bff1910300a39b1
Leonardo Carvalho <cleonardo191@gmail.com>
2024-10-29 13:12:00 +0000

    asdas

--- Moovie/src/components/Atoms/SortBy/SortBy.jsx
+++ Moovie/src/components/Atoms/SortBy/SortBy.jsx
@@ -4,7 +4,7 @@ const SortBy = ({ onChange }) => {
   return (
     <div className="title-genres">
       <h1>All Movies</h1>
-      <select onChange={onChange}>
+      <select className="selectStyle" onChange={onChange}>
         <option value="relevance">relevance</option>
         <option value="name">name</option>
         <option value="year">year</option>
