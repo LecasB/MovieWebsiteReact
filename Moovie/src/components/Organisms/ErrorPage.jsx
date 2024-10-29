378079618510f3172b3cff40aa130b04dc389f15
Leonardo <cleonardo191@gmail.com>
2024-10-29 00:27:04 +0000

    adas

--- Moovie/src/components/Organisms/ErrorPage.jsx
+++ Moovie/src/components/Organisms/ErrorPage.jsx
@@ -1,17 +1,35 @@
 import Layout from "../Molecules/Layout/Layout";
 import ImageError from "../../assets/Error404.gif";
 import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
 
 const ErrorPage = () => {
   return (
     <Layout>
       <div
-        style={{ display: "flex", flexDirection: "column", padding: "20px" }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "20px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
         <h1>Oops, This page does not exist</h1>
-        <img src={ImageError} />
-        <Link style={{ fontSize: "15px", padding: "10px 0px" }} to="/">
-          Return to Home Page
+        <img
+          style={{ height: "50%", width: "50%", maxWidth: "90vw" }}
+          src={ImageError}
+        />
+        <Link
+          style={{
+            padding: "10px 0px",
+            textDecoration: "underline",
+            display: "flex",
+            gap: "20px",
+          }}
+          to="/"
+        >
+          Return to Home Page <FaHome />
         </Link>
       </div>
     </Layout>
