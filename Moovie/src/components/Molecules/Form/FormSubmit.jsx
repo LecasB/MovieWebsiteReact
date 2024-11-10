1bbf6097d9b03a6a34b6b3dedb357a86f1ff84b6
Leonardo <cleonardo191@gmail.com>
2024-11-10 11:19:16 +0000

    drena

--- Moovie/src/components/Molecules/Form/FormSubmit.jsx
+++ Moovie/src/components/Molecules/Form/FormSubmit.jsx
@@ -3,6 +3,7 @@ import useFetch from "../../../hooks/Fetch/useFetch";
 import usePost from "../../../hooks/Post/usePost";
 import PopUpSubmit from "../PopUpSubmit/PopUpSubmit";
 import "./FormSubmit.css";
+import Button from "../../Atoms/Button/Button";
 
 const FormSubmit = ({ setReload = false }) => {
   const [data, isLoading, fetchErrorMessage] = useFetch(
@@ -10,7 +11,7 @@ const FormSubmit = ({ setReload = false }) => {
   );
 
   const { submitReview, isSubmitting, errorMessage, isSuccessful } = usePost();
-
+  const [visibibleForm, setVisibleForm] = useState(true);
   const [rating, setRating] = useState("");
   const [selectedMovieId, setSelectedMovieId] = useState("");
   const [selectedMovieTitle, setSelectedMovieTitle] = useState("");
@@ -40,8 +41,8 @@ const FormSubmit = ({ setReload = false }) => {
       last_name: e.target.lastName.value,
     };
 
-    setReviewData(newReviewData); // Store the review data
-    setIsPopupVisible(true); // Show the pop-up
+    setReviewData(newReviewData);
+    setIsPopupVisible(true);
   };
 
   const handleConfirm = async () => {
@@ -49,6 +50,7 @@ const FormSubmit = ({ setReload = false }) => {
     const success = await submitReview(reviewData);
     success && handleRefresh();
     resetForm();
+    setVisibleForm(!visibibleForm);
   };
 
   const handleCancel = () => {
@@ -70,111 +72,123 @@ const FormSubmit = ({ setReload = false }) => {
     <form ref={formRef} onSubmit={formHandle}>
       <div className="form-review">
         <h2>Submit Review</h2>
+        {visibibleForm && (
+          <>
+            <label htmlFor="reviewTitle">Title:</label>
+            <input
+              className="input-form-review"
+              type="text"
+              id="reviewTitle"
+              name="reviewTitle"
+              placeholder="Enter the title of your review"
+              required
+              minLength={5}
+              maxLength={50}
+            />
 
-        <label htmlFor="reviewTitle">Title:</label>
-        <input
-          className="input-form-review"
-          type="text"
-          id="reviewTitle"
-          name="reviewTitle"
-          placeholder="Enter the title of your review"
-          required
-          minLength={5}
-          maxLength={50}
-        />
+            <label htmlFor="reviewText">Review:</label>
+            <textarea
+              rows={8}
+              id="reviewText"
+              name="reviewText"
+              placeholder="Write your review here"
+              required
+              minLength={20}
+              maxLength={500}
+            />
 
-        <label htmlFor="reviewText">Review:</label>
-        <textarea
-          rows={8}
-          id="reviewText"
-          name="reviewText"
-          placeholder="Write your review here"
-          required
-          minLength={20}
-          maxLength={500}
-        />
-
-        <label htmlFor="moviesSelect">Select a Movie:</label>
-        <select
-          name="moviesSelect"
-          id="moviesSelect"
-          required
-          value={selectedMovieId}
-          onChange={(e) => {
-            setSelectedMovieId(e.target.value);
-            setSelectedMovieTitle(
-              e.target.options[e.target.selectedIndex].text
-            );
-          }}
-        >
-          <option value="" disabled>
-            Select a movie
-          </option>
-          {data &&
-            data.map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.title}
+            <label htmlFor="moviesSelect">Select a Movie:</label>
+            <select
+              className="input-form-review"
+              name="moviesSelect"
+              id="moviesSelect"
+              required
+              value={selectedMovieId}
+              onChange={(e) => {
+                setSelectedMovieId(e.target.value);
+                setSelectedMovieTitle(
+                  e.target.options[e.target.selectedIndex].text
+                );
+              }}
+            >
+              <option value="" disabled>
+                Select a movie
               </option>
-            ))}
-        </select>
+              {data &&
+                data.map((item) => (
+                  <option key={item.id} value={item.id}>
+                    {item.title}
+                  </option>
+                ))}
+            </select>
 
-        <label htmlFor="rating">Rating (1 to 5)</label>
-        <input
-          className="input-form-review"
-          type="number"
-          id="rating"
-          name="rating"
-          placeholder="Enter rating"
-          required
-          min={1}
-          max={5}
-          value={rating}
-          onChange={handleRatingChange}
-        />
+            <label htmlFor="rating">Rating (1 to 5)</label>
+            <input
+              className="input-form-review"
+              type="number"
+              id="rating"
+              name="rating"
+              placeholder="Enter rating"
+              required
+              min={1}
+              max={5}
+              value={rating}
+              onChange={handleRatingChange}
+            />
 
-        <label htmlFor="firstName">First Name:</label>
-        <input
-          className="input-form-review"
-          placeholder="John"
-          type="text"
-          id="firstName"
-          name="firstName"
-          required
-          minLength={2}
-          maxLength={30}
-        />
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              className="input-form-review"
+              placeholder="John"
+              type="text"
+              id="firstName"
+              name="firstName"
+              required
+              minLength={2}
+              maxLength={30}
+            />
 
-        <label htmlFor="lastName">Last Name:</label>
-        <input
-          className="input-form-review"
-          placeholder="Doe"
-          type="text"
-          id="lastName"
-          name="lastName"
-          required
-          minLength={2}
-          maxLength={30}
-        />
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              className="input-form-review"
+              placeholder="Doe"
+              type="text"
+              id="lastName"
+              name="lastName"
+              required
+              minLength={2}
+              maxLength={30}
+            />
 
-        <label htmlFor="email">Email:</label>
-        <input
-          className="input-form-review"
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Enter your email"
-          pattern="^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$"
-          required
-        />
+            <label htmlFor="email">Email:</label>
+            <input
+              className="input-form-review"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              pattern="^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$"
+              required
+            />
 
-        <button type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
 
-        {isSubmitting && <p>Submitting your review...</p>}
-        {errorMessage && <p>Error: {errorMessage}</p>}
-        {isSuccessful && <p>Review submitted successfully!</p>}
-        {fetchErrorMessage && <p>Error fetching movies: {fetchErrorMessage}</p>}
+            {isSubmitting && <p>Submitting your review...</p>}
+            {errorMessage && <p>Error: {errorMessage}</p>}
+            {isSuccessful && <p>Review submitted successfully!</p>}
+            {fetchErrorMessage && (
+              <p>Error fetching movies: {fetchErrorMessage}</p>
+            )}
+          </>
+        )}
+        {!visibibleForm && (
+          <Button
+            text={"New Review"}
+            onClick={() => setVisibleForm(!visibibleForm)}
+          />
+        )}
       </div>
 
       {isPopupVisible && (
